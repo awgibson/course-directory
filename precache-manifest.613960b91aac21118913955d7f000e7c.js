61b7ac4ca7f30959b3102a51b747bb8be4dcf009
awgibson <34604592+awgibson@users.noreply.github.com>
2019-04-28 14:21:27 -0500

    Updates

--- precache-manifest.613960b91aac21118913955d7f000e7c.js
+++ precache-manifest.613960b91aac21118913955d7f000e7c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/course-directory/static/js/runtime~main.527751ac.js"
   },
   {
-    "revision": "3529578aa73fb3462811",
-    "url": "/course-directory/static/js/main.3529578a.chunk.js"
+    "revision": "fc24c3fc07d7e097f6fe",
+    "url": "/course-directory/static/js/main.fc24c3fc.chunk.js"
   },
   {
     "revision": "46c28677fdb8bfdbc317",
     "url": "/course-directory/static/js/1.46c28677.chunk.js"
   },
   {
-    "revision": "3529578aa73fb3462811",
+    "revision": "fc24c3fc07d7e097f6fe",
     "url": "/course-directory/static/css/main.d7938ea6.chunk.css"
   },
   {
-    "revision": "461b644b912a0ed756cc6be16096bfc3",
+    "revision": "59b1b5beb79d3a3036b483af2182e0d9",
     "url": "/course-directory/index.html"
   }
 ];
