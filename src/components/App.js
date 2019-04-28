74490944bdd09b99b574cdc5ddba4905e975fe8d
awgibson <34604592+awgibson@users.noreply.github.com>
2019-04-28 14:17:05 -0500

    hash router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch
-} from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 // App components
 import Header from './Header';
@@ -15,24 +11,20 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <div className="container">
       <Header />
-      
+
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" render={ () => <About title='About' /> } />
+        <Route path="/about" render={() => <About title="About" />} />
         <Route exact path="/teachers" component={Teachers} />
         <Route path="/teachers/:topic/:name" component={Featured} />
         <Route path="/courses" component={Courses} />
         <Route component={NotFound} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
-
-
-
-
